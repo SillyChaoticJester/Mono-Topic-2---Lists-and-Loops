89529b308974f803416abcf9d14d727dd34a913a
Miracle McMahon <339663114@GoTVDSB.ca>
2025-03-31 14:31:36 -0400

    Going through the tutorial

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace Mono_Topic_2___Lists_and_Loops
 {
@@ -8,6 +10,12 @@ namespace Mono_Topic_2___Lists_and_Loops
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Random generator;
+        Rectangle window;
+        Texture2D spaceBackgroundTexture;
+        List<Texture2D> textures;
+        List<Rectangle> planetRects;
 
         public Game1()
         {
@@ -20,6 +28,20 @@ namespace Mono_Topic_2___Lists_and_Loops
         {
             // TODO: Add your initialization logic here
 
+            window = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+            generator = new Random();
+            textures = new List<Texture2D>();
+            planetRects = new List<Rectangle>();
+
+            for (int i = 0; i < 30; i++)
+            {
+                planetRects.Add
+                (
+                    new Rectangle(generator.Next(window.Width - 25), generator.Next(window.Height - 25), 25, 25)
+                );
+            }
+
+
             base.Initialize();
         }
 
@@ -28,6 +50,13 @@ namespace Mono_Topic_2___Lists_and_Loops
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            spaceBackgroundTexture = Content.Load<Texture2D>("Images/space_background");
+            for (int i = 1; i <= 13; i++)
+            {
+                textures.Add(Content.Load<Texture2D>("Images/16-bit-planet" + i));
+            }
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,6 +74,13 @@ namespace Mono_Topic_2___Lists_and_Loops
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(spaceBackgroundTexture, window, Color.White);
+            for (int i = 0; i < planetRects.Count; i++)
+            {
+                _spriteBatch.Draw(textures[0], planetRects[i], Color.White);
+            }
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
